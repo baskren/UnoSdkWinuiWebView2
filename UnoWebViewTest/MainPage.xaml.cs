27741bb3e5254a810f4cb4b9cb80cfc305f4bb35
baskren <ben@buildcalc.com>
2024-10-23 07:37:48 -0400

    Initial Commit

---
+++ UnoWebViewTest/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+namespace UnoWebViewTest;
+
+public sealed partial class MainPage : Page
+{
+    public MainPage()
+    {
+        this.InitializeComponent();
+
+        //MyWebView.Source = new Uri("ms-appx:///Assets/Html/cbracco.html");
+    }
+}
